b7f1139c439d2de71654a96b3144ee940ae0fd89
vengardus <ismytv@gmail.com>
2023-10-11 22:56:58 -0500

    feat:jwt with jose v.test

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { type RequestEventBase } from "@builder.io/qwik-city";
 import { type IUserEntity } from "~/domain/entity/user.entity";
 import { Auth } from "~/domain/model/auth.model";
+import { SignJWT } from "jose";
 
 export class AuthController {
     constructor(private requestEvent: RequestEventBase) { }
@@ -16,15 +17,32 @@ export class AuthController {
         const oAuth = new Auth()
         const response = await oAuth.login(this.requestEvent, dataAuth)
 
+        if (response.success) {
+            // const secret = new TextEncoder().encode(
+            //     'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
+            // )
+            const secret = new TextEncoder().encode(
+                this.requestEvent.env.get('APP_TOKEN_KEY')
+            )
+            const alg = 'HS256'
+            const data = {
+                id: response.user?.id,
+                usrname: response.user?.username
+            }
+            const jwt = await new SignJWT(data)
+                .setProtectedHeader({ alg })
+                .setIssuedAt()
+                .setExpirationTime('2h')
+                .sign(secret)
 
+            console.log(jwt)
 
-
-        if (response.success)
             this.requestEvent.cookie.set(
                 'app-token',
-                this.requestEvent.env.get('APP_TOKEN_KEY')!,
+                jwt,
                 { secure: true, path: '/' }
             )
+        }
 
         return response
     }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikVite } from "@builder.io/qwik/optimizer"; 
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
